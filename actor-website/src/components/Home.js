76b0588a046089dde46abd5e37e1efc9b9311b9c
Blake Sartin <blake.sartin8@gmail.com>
2022-11-01 23:13:54 -0700

    add skeleton for home page

---
+++ actor-website/src/components/Home.js
@@ -0,0 +1,17 @@
+import React from "react"
+import "./Home.scss"
+
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <div className="title-container">
+    <h1 className="title"></h1>
+    <h2 className="welcome-statement"></h2>
+    </div>
+    <div className="home-photo-container">
+      <img></img>
+      </div>
+    </div>
+  )
+}
