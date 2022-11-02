509847ed943884c17ea449f5697f835641297c77
Blake Sartin <blake.sartin8@gmail.com>
2022-11-01 23:23:05 -0700

    add skeleton for about page

---
+++ actor-website/src/components/About.js
@@ -0,0 +1,18 @@
+import React from "react"
+import "./About.scss"
+
+export default function About() {
+  return (
+    <div className="about-container">
+      <div className="bio-container">
+        <h2 className="bio"></h2>
+        </div>
+        <div className="info-container">
+          <h2 className="info"></h2>
+          </div>
+          <div className="about-picture-container">
+            <img></img>
+            </div>
+      </div>
+  )
+}
--- actor-website/src/components/Home.js
+++ actor-website/src/components/Home.js
@@ -1,17 +1,19 @@
-import React from "react"
-import "./Home.scss"
-
+import React from "react";
+import "./Home.scss";
 
 export default function Home() {
   return (
     <div className="home-container">
       <div className="title-container">
-    <h1 className="title"></h1>
-    <h2 className="welcome-statement"></h2>
-    </div>
-    <div className="home-photo-container">
-      <img></img>
+        <h1 className="title"></h1>
+        <h2 className="welcome-statement"></h2>
+      </div>
+      <div className="home-photo-container">
+        <img></img>
+      </div>
+      <div className="news-container">
+        <h2 className="news"></h2>
       </div>
     </div>
-  )
+  );
 }
