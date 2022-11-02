e7835b6b352f80431387b6cfdf40ee7348eaab85
Blake Sartin <blake.sartin8@gmail.com>
2022-11-01 23:27:07 -0700

    add contact.js skeleton

---
+++ actor-website/src/components/Contact.js
@@ -0,0 +1,17 @@
+import React from "react"
+import "./Contact.scss"
+
+export default function Contact() {
+  return (
+    <div className="contact-container">
+      <div className="contact-message-container">
+        <h1 className="contact-message"></h1>
+        <form>
+          <input></input>
+          <textarea></textarea>
+          <button type="submit"></button>
+        </form>
+        </div>
+      </div>
+  )
+}
