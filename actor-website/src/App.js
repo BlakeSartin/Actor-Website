0cb30852fa57d46c1ef24fea12389a0f5ba7eb16
Blake Sartin <blake.sartin8@gmail.com>
2022-11-03 23:34:15 -0700

    add switch statement to app.js and topbar

--- actor-website/src/App.js
+++ actor-website/src/App.js
@@ -1,9 +1,38 @@
-
-import './App.css';
+import "./App.css";
+import About from "./components/About";
+import Home from "./components/Home";
+import Credits from "./components/Credits";
+import Reels from "./components/Reels";
+import Pictures from "./components/Pictures";
+import Contact from "./components/Contact";
+import Topbar from "./components/Topbar";
 
 function App() {
+  let component;
+  switch (window.location.pathname) {
+    case "/":
+      component = <Home />;
+      break;
+    case "/about":
+      component = <About />;
+      break;
+    case "/credits":
+      component = <Credits />;
+      break;
+    case "/reels":
+      component = <Reels />;
+      break;
+    case "/pictures":
+      component = <Pictures />;
+      break;
+    case "/contact":
+      component = <Contact />;
+      break;
+  }
   return (
-    <div className="App">
+    <div>
+      <Topbar />
+      {component}
     </div>
   );
 }
