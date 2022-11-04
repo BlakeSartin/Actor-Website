31869e498382ee2b33f60fa6af6920b92ac0cf07
Blake Sartin <blake.sartin8@gmail.com>
2022-11-04 16:54:17 -0700

    add video and beggining of styling

--- actor-website/src/components/Home.js
+++ actor-website/src/components/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./Home.scss";
+import video from '../videos/video1.mp4'
+
 
 export default function Home() {
   return (
     <div className="home-container">
       <div className="home-photo-container">
-        <img></img>
-        image
+        <video className="video1" controls width="100%" height="100%" src={video} autoPlay loop muted >
+        </video>
       </div>
       <div className="title-container">
         <h2 className="welcome-statement">Hello</h2>
