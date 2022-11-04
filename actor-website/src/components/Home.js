311021abfb5f3e21160807a6aa3cbde253786398
Blake Sartin <blake.sartin8@gmail.com>
2022-11-04 15:34:19 -0700

    add container sizing styling

--- actor-website/src/components/Home.js
+++ actor-website/src/components/Home.js
@@ -4,15 +4,15 @@ import "./Home.scss";
 export default function Home() {
   return (
     <div className="home-container">
-      <div className="title-container">
-        <h1 className="title"></h1>
-        <h2 className="welcome-statement"></h2>
-      </div>
       <div className="home-photo-container">
         <img></img>
+        image
+      </div>
+      <div className="title-container">
+        <h2 className="welcome-statement">Hello</h2>
       </div>
       <div className="news-container">
-        <h2 className="news"></h2>
+        <h2 className="news">News</h2>
       </div>
     </div>
   );
