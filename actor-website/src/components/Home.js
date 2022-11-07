40d208bc400135b85aaee8e2d1db2e394d6a550a
Blake Sartin <blake.sartin8@gmail.com>
2022-11-06 19:18:51 -0800

    add spacing for photo container

--- actor-website/src/components/Home.js
+++ actor-website/src/components/Home.js
@@ -1,16 +1,17 @@
 import React from "react";
 import "./Home.scss";
 
-
-
 export default function Home() {
   return (
     <div className="home-container">
       <div className="home-photo1-container">
+        <img></img>
       </div>
       <div className="home-photo2-container">
+        <img></img>
       </div>
       <div className="home-photo3-container">
+        <img></img>
       </div>
     </div>
   );
