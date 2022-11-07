ffb79fa8e9503e1f9b28784ecd8e4e69a3e9e7b0
Blake Sartin <blake.sartin8@gmail.com>
2022-11-06 19:15:19 -0800

    add new styling

--- actor-website/src/components/Home.js
+++ actor-website/src/components/Home.js
@@ -1,20 +1,16 @@
 import React from "react";
 import "./Home.scss";
-import video from '../videos/video1.mp4'
+
 
 
 export default function Home() {
   return (
     <div className="home-container">
-      <div className="home-photo-container">
-        <video className="video1" controls width="100%" height="100%" src={video} autoPlay loop muted >
-        </video>
+      <div className="home-photo1-container">
       </div>
-      <div className="title-container">
-        <h2 className="welcome-statement">Hello</h2>
+      <div className="home-photo2-container">
       </div>
-      <div className="news-container">
-        <h2 className="news">News</h2>
+      <div className="home-photo3-container">
       </div>
     </div>
   );
