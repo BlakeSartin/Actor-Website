e2a77efda4e144dde935dc5ac35551c220ae41b5
Blake Sartin <blake.sartin8@gmail.com>
2022-11-06 19:38:46 -0800

    add photos for home screen

--- actor-website/src/components/Home.js
+++ actor-website/src/components/Home.js
@@ -1,17 +1,18 @@
 import React from "react";
 import "./Home.scss";
 
+
 export default function Home() {
   return (
     <div className="home-container">
       <div className="home-photo1-container">
-        <img></img>
+        <img className="home-photo1" src="../photos/photo1.JPG" ></img>
       </div>
       <div className="home-photo2-container">
-        <img></img>
+        <img className="home-photo2" src="../photos/photo2.jpg"></img>
       </div>
       <div className="home-photo3-container">
-        <img></img>
+        <img className="home-photo3" src="../photos/photo3.jpg"></img>
       </div>
     </div>
   );
