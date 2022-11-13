61ab341ac56f9aaaa39411a6630647ffff0ff055
Blake Sartin <blake.sartin8@gmail.com>
2022-11-12 18:37:58 -0800

    add fake data for credits page

--- actor-website/src/components/Credits.js
+++ actor-website/src/components/Credits.js
@@ -1,6 +1,25 @@
 import React from "react"
 import "./Credits.scss"
 
+let data = [
+  {id: "1",
+  title:"TV Show",
+  img:"../photos/photo1.JPG"
+},
+{id: "1",
+  title:"TV Show",
+  img:"../photos/photo2.jpg"
+},
+{id: "1",
+  title:"TV Show",
+  img:"../photos/photo3.jpg"
+},
+{id: "1",
+  title:"TV Show",
+  img:"../photos/photo3.jpg"
+},
+]
+
 export default function Credits() {
   return (
     <div className="credits-container">
