22059cfe5d35bb2227177bd1f67bd69f0b2f16b5
Blake Sartin <blake.sartin8@gmail.com>
2022-11-12 18:42:36 -0800

    add function for navigating slides in credits component

--- actor-website/src/components/Credits.js
+++ actor-website/src/components/Credits.js
@@ -1,28 +1,22 @@
-import React from "react"
-import "./Credits.scss"
+import React, { useState } from "react";
+import "./Credits.scss";
 
-let data = [
-  {id: "1",
-  title:"TV Show",
-  img:"../photos/photo1.JPG"
-},
-{id: "1",
-  title:"TV Show",
-  img:"../photos/photo2.jpg"
-},
-{id: "1",
-  title:"TV Show",
-  img:"../photos/photo3.jpg"
-},
-{id: "1",
-  title:"TV Show",
-  img:"../photos/photo3.jpg"
-},
-]
 
 export default function Credits() {
-  return (
-    <div className="credits-container">
-      </div>
-  )
+  const [currentSlide, setCurrentSlide] = useState(0)
+
+  let data = [
+    { id: "1", title: "TV Show", img: "../photos/photo1.JPG" },
+    { id: "1", title: "TV Show", img: "../photos/photo2.jpg" },
+    { id: "1", title: "TV Show", img: "../photos/photo3.jpg" },
+    { id: "1", title: "TV Show", img: "../photos/photo3.jpg" },
+  ];
+  
+  const handleClick = (direction) => {
+    direction === "left"
+      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : data.length - 1)
+      : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
+  };
+
+  return <div className="credits-container"></div>;
 }
