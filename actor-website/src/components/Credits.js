5dca59d376bbf94a5b0270848a67bb010fdb595e
Blake Sartin <blake.sartin8@gmail.com>
2022-11-13 22:27:04 -0800

    add more styling for credits component

--- actor-website/src/components/Credits.js
+++ actor-website/src/components/Credits.js
@@ -1,17 +1,36 @@
 import React, { useState } from "react";
 import "./Credits.scss";
 
-
 export default function Credits() {
-  const [currentSlide, setCurrentSlide] = useState(0)
+  const [currentSlide, setCurrentSlide] = useState(0);
 
   let data = [
-    { id: "1", title: "TV Show", desc:"This is a description of a tv show that Katrina Teitz has starred in!", img: "../photos/photo1.JPG" },
-    { id: "1", title: "TV Show", desc:"This is a description of a tv show that Katrina Teitz has starred in!", img: "../photos/photo2.jpg" },
-    { id: "1", title: "TV Show", desc:"This is a description of a tv show that Katrina Teitz has starred in!", img: "../photos/photo3.jpg" },
-    { id: "1", title: "TV Show", desc:"This is a description of a tv show that Katrina Teitz has starred in!", img: "../photos/photo3.jpg" },
+    {
+      id: "1",
+      title: "TV Show",
+      desc: "This is a description of a tv show that Katrina Teitz has starred in!",
+      img: "../photos/photo1.JPG",
+    },
+    {
+      id: "1",
+      title: "TV Show",
+      desc: "This is a description of a tv show that Katrina Teitz has starred in!",
+      img: "../photos/photo2.jpg",
+    },
+    {
+      id: "1",
+      title: "TV Show",
+      desc: "This is a description of a tv show that Katrina Teitz has starred in!",
+      img: "../photos/photo3.jpg",
+    },
+    {
+      id: "1",
+      title: "TV Show",
+      desc: "This is a description of a tv show that Katrina Teitz has starred in!",
+      img: "../photos/photo3.jpg",
+    },
   ];
-  
+
   const handleClick = (direction) => {
     direction === "left"
       ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : data.length - 1)
@@ -19,25 +38,26 @@ export default function Credits() {
   };
 
   return (
-  <div className="credits-container">
-    <div className="slider" style={{transform: `translateX(-${currentSlide*100}vw)`}}>
-      {data.map((d) => (
-        <div className="credits-img-container">
-          <div className="item">
-            <div className="left">
-              <div className="left-container">
-                <div className="img-container">
-                  {d.desc}
-                  </div>
+    <div className="credits-container">
+      <div
+        className="slider"
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
+        {data.map((d) => (
+          <div className="credits-img-container">
+            <div className="item">
+              <div className="left">
+                <div className="left-container">
+                  <div className="desc-container">{d.desc}</div>
                 </div>
               </div>
               <div className="right">
-                <img src={d.img} alt=''/>
+                <img src={d.img} alt="" />
+              </div>
             </div>
           </div>
-          </div>
-      ))}
+        ))}
       </div>
     </div>
-      );
+  );
 }
