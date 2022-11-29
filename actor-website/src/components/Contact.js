a9fa711fb1f1c3f2b963bec3a1aa7008e6b4e906
Blake Sartin <blake.sartin8@gmail.com>
2022-11-29 13:45:13 -0800

    add contact component styling

--- actor-website/src/components/Contact.js
+++ actor-website/src/components/Contact.js
@@ -1,12 +1,13 @@
 import React from "react"
 import "./Contact.scss"
+import News from "./News"
 
 export default function Contact() {
   return (
     <div className="contact-container">
       <div className="contact-message-container">
-        <h1 className="contact-message"></h1>
-        <form>
+        <h1 className="contact-message">Hello this is a message</h1>
+        <form className="contact-form">
           <input></input>
           <textarea></textarea>
           <button type="submit"></button>
