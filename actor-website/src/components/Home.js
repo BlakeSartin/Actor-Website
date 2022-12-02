970e891dd8b0cd2705a3148bb8715e38de00430b
Blake Sartin <blake.sartin8@gmail.com>
2022-12-02 14:28:34 -0800

    add picture to home screen

--- actor-website/src/components/Home.js
+++ actor-website/src/components/Home.js
@@ -9,8 +9,10 @@ export default function Home() {
       <img className="home-photo1" src="../photos/photo1.JPG"></img>
       <img className="home-photo2" src="../photos/photo2.jpg"></img>
       <img className="home-photo3" src="../photos/photo3.jpg"></img>
+      <img className="home-photo4" src="../photos/pastelsplatter2.png"></img>
       </div>
       <div className="home-title-container">
+      
         <h1 className="actor">Actor</h1>
         <h1 className="stunt-performer">Stunt Performer</h1>
         <h1 className="social-media">Social Media Coordinator</h1>
