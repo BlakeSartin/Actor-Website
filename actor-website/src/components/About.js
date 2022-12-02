111b8a8c48116eee4f191bcfcb23b19bc34afc43
Blake Sartin <blake.sartin8@gmail.com>
2022-12-01 22:56:23 -0800

    add bio and info on about page

--- actor-website/src/components/About.js
+++ actor-website/src/components/About.js
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <div className="about-container">
       <div className="bio-container">
-        <h2 className="bio"></h2>
+        <h2 className="bio">Katrina Teitz is an actor/singer/dancer based in Vancouver, BC. She has been in several shows around town after receiving her Diploma in Musical Theatre from Capilano University (2017). Her favourite credits include Next to Normal (West Moon Theatre), Crybaby the Musical (Awkward Stage Productions), Kill The Ripper (Geekenders/Affair of Honor). She is an aspiring stunt performer with training in flips, tricks, Wushu and a black belt in Karate. Katrina is also a proud cat mama of two. You can find her on all socials at @Katrinateitz</h2>
         </div>
         <div className="info-container">
           <h2 className="info"></h2>
