ec95e1b7785e34db479e4a8952f20693a9fc6481
Blake Sartin <blake.sartin8@gmail.com>
2022-12-03 13:15:39 -0800

    changed styling for home page

--- actor-website/src/components/Home.js
+++ actor-website/src/components/Home.js
@@ -6,18 +6,17 @@ export default function Home() {
   return (
     <div className="home-container">
       <div className="home-photo-container">
-      <img className="home-photo1" src="../photos/photo1.JPG"></img>
-      <img className="home-photo2" src="../photos/photo2.jpg"></img>
-      <img className="home-photo3" src="../photos/photo3.jpg"></img>
-      <img className="home-photo4" src="../photos/pastelsplatter2.png"></img>
+        <img className="home-photo1" src="../photos/photo1.JPG"></img>
+        <img className="home-photo2" src="../photos/photo2.jpg"></img>
+        <img className="home-photo3" src="../photos/photo3.jpg"></img>
       </div>
       <div className="home-title-container">
-      
         <h1 className="actor">Actor</h1>
         <h1 className="stunt-performer">Stunt Performer</h1>
         <h1 className="social-media">Social Media Coordinator</h1>
-        </div>
-        <News/>
+      </div>
+      <News />
+      <img className="home-photo4" src="../photos/paintsplatter3.png"></img>
     </div>
   );
 }
