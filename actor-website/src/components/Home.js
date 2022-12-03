9ffed71626a334df823186510895475e1c217d27
Blake Sartin <blake.sartin8@gmail.com>
2022-12-03 13:34:59 -0800

    add picture to home screen and styling

--- actor-website/src/components/Home.js
+++ actor-website/src/components/Home.js
@@ -6,6 +6,8 @@ export default function Home() {
   return (
     <div>
       <img className="home-photo4" src="../photos/paintsplatter4.png"></img>
+      <img className="home-photo5" src="../photos/cyanpaint.png"></img>
+
     <div className="home-container">
       <div className="home-photo-container">
         <img className="home-photo1" src="../photos/photo1.JPG"></img>
