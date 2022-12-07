837f2a2f9b75fc7c0d9f7897be639d1467f1be8c
Blake Sartin <blake.sartin8@gmail.com>
2022-12-07 13:18:51 -0800

    add function to handle submission on contact page and form

--- actor-website/src/components/Contact.js
+++ actor-website/src/components/Contact.js
@@ -1,18 +1,30 @@
-import React from "react"
-import "./Contact.scss"
-import News from "./News"
+import React, {useState} from "react";
+import "./Contact.scss";
 
 export default function Contact() {
+  const [submitting, setSubmitting] = useState(false);
+    const handleSubmit = event => {
+      event.preventDefault();
+     setSubmitting(true);
+  
+     setTimeout(() => {
+       setSubmitting(false);
+     }, 3000)
+   }
+
   return (
-    <div className="contact-container">
-      <div className="contact-message-container">
-        <h1 className="contact-message">Hello this is a message</h1>
-        <form className="contact-form">
-          <input></input>
-          <textarea></textarea>
-          <button type="submit"></button>
-        </form>
-        </div>
-      </div>
-  )
+    <div className="wrapper">
+      <h1>How About Them Apples</h1>
+      {submitting && <div>Submtting Form...</div>}
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label>
+            <p>Name</p>
+            <input name="name" />
+          </label>
+        </fieldset>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
