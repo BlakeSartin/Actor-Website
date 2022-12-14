55c5c86d46dad45e4ff7e565a25083104eb89be8
Blake Sartin <blake.sartin8@gmail.com>
2022-12-13 20:13:54 -0800

    started to style picture page

--- actor-website/src/components/Pictures.js
+++ actor-website/src/components/Pictures.js
@@ -5,7 +5,17 @@ export default function Pictures() {
   return (
     <div className="pictures-container">
       <div className="pic-container">
-        <img></img>
+      <img className="home-photo1" src="../photos/photo1.JPG"></img>
+        <h2 className="photo-description"></h2>
+        <h3 className="photo-author"></h3>
+      </div>
+      <div className="pic-container">
+      <img className="home-photo1" src="../photos/photo1.JPG"></img>
+        <h2 className="photo-description"></h2>
+        <h3 className="photo-author"></h3>
+      </div>
+      <div className="pic-container">
+      <img className="home-photo1" src="../photos/photo1.JPG"></img>
         <h2 className="photo-description"></h2>
         <h3 className="photo-author"></h3>
       </div>
