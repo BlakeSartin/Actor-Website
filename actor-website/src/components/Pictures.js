5d9006629530b2f2117dc6106c8501081ac7ea30
Blake Sartin <blake.sartin8@gmail.com>
2022-12-15 23:58:28 -0800

    add more styling for picture component

--- actor-website/src/components/Pictures.js
+++ actor-website/src/components/Pictures.js
@@ -47,7 +47,7 @@ export default function Pictures() {
         <h2 className="photo-description">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu
           ultrices purus, convallis pulvinar sem. Pellentesque dignissim
-          suscipit luctus. Nam sed nisi ut sem laoreet rhoncus sit amet vitae
+     cd      suscipit luctus. Nam sed nisi ut sem laoreet rhoncus sit amet vitae
           lacus. Mauris posuere maximus eleifend. Vivamus ac justo purus.
           Suspendisse sodales dolor eget libero lacinia, sed cursus erat
           molestie. Cras consectetur leo nisi, et molestie risus tincidunt
@@ -60,6 +60,26 @@ export default function Pictures() {
           ultricies eget in odio. Duis ac lorem sit amet eros cursus commodo et
           a justo. Duis imperdiet elit sit amet pellentesque venenatis.
         </h2>
+      </div>
+      <div className="pic-container4">
+        <img className="pictures-photo1" src="../photos/photo1.JPG"></img>
+        <h2 className="photo-description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu
+          ultrices purus, convallis pulvinar sem. Pellentesque dignissim
+     cd      suscipit luctus. Nam sed nisi ut sem laoreet rhoncus sit amet vitae
+          lacus. Mauris posuere maximus eleifend. Vivamus ac justo purus.
+          Suspendisse sodales dolor eget libero lacinia, sed cursus erat
+          molestie. Cras consectetur leo nisi, et molestie risus tincidunt
+          vestibulum. Nullam tristique dolor et semper luctus. Curabitur id
+          sapien congue, finibus ipsum nec, pretium mauris. Orci varius natoque
+          penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+          Aliquam feugiat tellus ex, eu dictum sapien scelerisque ac. Praesent
+          porta mauris ut dolor eleifend, sit amet vulputate ipsum luctus. Fusce
+          eu tincidunt mi, quis pulvinar nisi. Nulla ut urna eget velit cursus
+          ultricies eget in odio. Duis ac lorem sit amet eros cursus commodo et
+          a justo. Duis imperdiet elit sit amet pellentesque venenatis.
+        </h2>
+        <img className="pictures-photo1" src="../photos/photo1.JPG"></img>
       </div>
     </div>
   );
