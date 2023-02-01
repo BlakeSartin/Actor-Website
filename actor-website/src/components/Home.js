107cb4d7f436489ef380c54911665a12f9149170
Blake Sartin <blake.sartin8@gmail.com>
2023-01-31 17:48:12 -0800

    changed pictures on home screen

--- actor-website/src/components/Home.js
+++ actor-website/src/components/Home.js
@@ -10,9 +10,9 @@ export default function Home() {
 
     <div className="home-container">
       <div className="home-photo-container">
-        <img className="home-photo1" src="../photos/photo1.JPG"></img>
-        <img className="home-photo2" src="../photos/photo2.jpg"></img>
-        <img className="home-photo3" src="../photos/photo3.jpg"></img>
+        <img className="home-photo1" src="../photos/katrina1.jpg"></img>
+        <img className="home-photo2" src="../photos/katrina2.jpg"></img>
+        <img className="home-photo3" src="../photos/katrina3.jpg"></img>
       </div>
       <div className="home-title-container">
         <h1 className="actor">Actor</h1>
