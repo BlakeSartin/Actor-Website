100e8a1a78bfeec24da6a5863abe20200831ae37
Blake Sartin <blake.sartin8@gmail.com>
2023-02-14 17:01:49 -0800

    add pictures to project

--- actor-website/src/components/Pictures.js
+++ actor-website/src/components/Pictures.js
@@ -6,11 +6,13 @@ export default function Pictures() {
   return (
     <div className="pictures-container">
       <div className="gallery_container">
-      <img className="pictures_photo" src="../photos/katrina1.jpg"></img>
-      <img className="pictures_photo" src="../photos/katrina1.jpg"></img>
-      <img className="pictures_photo" src="../photos/katrina1.jpg"></img>
-      <img className="pictures_photo" src="../photos/katrina1.jpg"></img>
-      <img className="pictures_photo" src="../photos/katrina1.jpg"></img>
+      <img className="pictures_photo1" src="../photos/KT_VitaeApparel(1).jpg"></img>
+      <img className="pictures_photo2" src="../photos/kill_the_ripper2.jpg"></img>
+      <img className="pictures_photo3" src="../photos/The Body Of My Name 名自字体 - 12.jpg"></img>
+      <img className="pictures_photo4" src="../photos/bdt_0004.jpg"></img>
+      <img className="pictures_photo5" src="../photos/KatrinaTeitz(1).jpeg"></img>
+      <img className="pictures_photo6" src="../photos/KatrinaTeitz(2).jpeg"></img>
+      
       </div>
     </div>
   );
