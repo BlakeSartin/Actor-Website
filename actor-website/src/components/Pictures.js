04bd03b6493fb3070b6930d67cc4efb30e9baaa3
Blake Sartin <blake.sartin8@gmail.com>
2023-02-14 16:43:50 -0800

    changes to pictures component

--- actor-website/src/components/Pictures.js
+++ actor-website/src/components/Pictures.js
@@ -5,7 +5,13 @@ import News from "./News"
 export default function Pictures() {
   return (
     <div className="pictures-container">
-      <News/>
+      <div className="gallery_container">
+      <img className="pictures_photo" src="../photos/katrina1.jpg"></img>
+      <img className="pictures_photo" src="../photos/katrina1.jpg"></img>
+      <img className="pictures_photo" src="../photos/katrina1.jpg"></img>
+      <img className="pictures_photo" src="../photos/katrina1.jpg"></img>
+      <img className="pictures_photo" src="../photos/katrina1.jpg"></img>
+      </div>
     </div>
   );
 }
